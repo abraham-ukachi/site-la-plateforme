85bf8d1e3bcff05f88a835d930366505e811bf3a
abraham-ukachi <abraham.ukachi@laplateforme.io>
2022-10-30 22:14:10 +0100

    Update laplateforme.css

---
+++ laplateforme.js
@@ -0,0 +1,9 @@
+/**
+ * Method that is called whenever laPlateforme is ready to be used.
+ * This happens after the progress bar element in the Splash Screen reaches 100.
+ */
+laPlateforme.onReady = () => {
+  
+  // DEBUG [4dbsmaster]: tell me about it :)
+  console.log(`[laPlateforme] (onReady): \x1b[34mI am ready!!!\x1b[0m`);
+}
